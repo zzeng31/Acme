5c38b4ef4eb2b05380a8d87905926780993d86e5
zzeng31 <zinuozeng@gmail.com>
2024-01-20 14:21:41 -0800

    init

---
+++ app/ui/invoices/status.tsx
@@ -0,0 +1,27 @@
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+
+export default function InvoiceStatus({ status }: { status: string }) {
+  const style = 'inline-flex items-center rounded-full px-2 py-1 text-xs';
+  return (
+    <span
+      className={clsx(style, {
+        'bg-gray-100 text-gray-500': status === 'pending',
+        'bg-green-500 text-white': status === 'paid',
+      })}
+    >
+      {status === 'pending' ? (
+        <>
+          Pending
+          <ClockIcon className="ml-1 w-4 text-gray-500" />
+        </>
+      ) : null}
+      {status === 'paid' ? (
+        <>
+          Paid
+          <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+    </span>
+  );
+}
